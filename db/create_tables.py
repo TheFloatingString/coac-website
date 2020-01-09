7a6b654c6dcc12b8c3f7a9daf4f8b381decd2237
The Floating String <laurencel2001@gmail.com>
2020-01-09 15:48:43 -0500

    Implemented workable events submission and table

--- db/create_tables.py
+++ db/create_tables.py
@@ -6,16 +6,21 @@ import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import bcrypt
+import uuid
+
+import random
 
 engine = create_engine(os.environ["DATABASE_URL"], echo=True)
 
 meta = MetaData()
 Base = declarative_base()
 
+print("tables loaded")
+
 class Event(Base):
 	__tablename__ = "events"
 
-	id = Column(Integer, primary_key=True, unique=True, nullable=False, default=int(uuid.uuid4()))
+	id = Column(Integer, primary_key=True, unique=True, nullable=False, default=random.randint(0,2**30))
 	name = Column(String, nullable=False)
 	creator_id = Column(Integer)
 	creator_name = Column(String)
@@ -60,7 +65,7 @@ class User(Base):
 	def set_password(self, password):
 		self.password_hash = generate_password_hash
 
-	def check_password(self, password)
+	def check_password(self, password):
 		return check_password_hash(password, self.password_hash)
 
 
